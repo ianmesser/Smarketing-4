604d0dc2bc69e83069e2ab3754557098fb07ccc8
ianmesser <ian.david.messer@gmail.com>
2025-05-29 16:55:13 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import VendorDashboard from "./vendordashboard";
+import RetailerDashboard from "./RetailerDashboard";
 
 function App() {
   return (
     <div className="min-h-screen bg-white text-gray-900 p-4">
       <h1 className="text-2xl font-bold mb-4">Ad Placement Marketplace</h1>
-      <VendorDashboard />
+      <RetailerDashboard />
     </div>
   );
 }
