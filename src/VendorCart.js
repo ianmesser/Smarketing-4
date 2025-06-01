884048a937a724941772fbbfa6882112bdc45d2f
ianmesser <ian.david.messer@gmail.com>
2025-06-01 16:15:01 -0700

    Update VendorCart.js

--- src/VendorCart.js
+++ src/VendorCart.js
@@ -59,7 +59,7 @@ const VendorCart = () => {
     }
   
     const purchasesToInsert = cart.map((item) => ({
-      vendor_id: "demo-id-or-null", // Replace with actual vendor logic when ready
+      vendor_id: "00000000-0000-4000-8000-000000000000", // Replace with actual vendor logic when ready
       availability_id: item.availabilityId,
     }));
   
